29e0d4acbb849f9028a0f9272da9103c7fdab5a6
krystian-badowski <krystian.badowski@badek.org>
2024-03-14 16:53:35 +0100

    fix hover, hamburger, urls, footer align, favicon

--- html/src/js/script.js
+++ html/src/js/script.js
@@ -6,5 +6,39 @@ function handleScroll() {
       topbar.classList.remove('scrolled');
     }
   }
-  window.addEventListener('scroll', handleScroll);
+window.addEventListener('scroll', handleScroll);
 
+
+
+var scrollLinks = document.querySelectorAll('a[href^="#"]');
+
+scrollLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+        var targetId = this.getAttribute("href").substring(1);
+        var targetElement = document.getElementById(targetId);
+        console.log(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 200,
+                behavior: "smooth",
+            });
+
+            event.preventDefault();
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector('.hamburger');
+  const menuList = document.querySelector('.menu__list');
+
+  hamburger.addEventListener('click', function() {
+      if (!menuList.classList.contains('openmenu')) {
+          menuList.classList.add('openmenu');
+      } else {
+          menuList.classList.remove('openmenu');
+      }
+  });
+});
